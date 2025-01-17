da8c554668b2055feee04bbe763924fd622a5eff
Mohak <mohaksharma34@gmail.com>
2025-01-17 14:54:45 +0500

    Added the needed libraries, zipped as a package.

--- NumberGuessingGame.cpp
+++ NumberGuessingGame.cpp
@@ -5,7 +5,7 @@ Develop a number guessing game where the
 computer randomly selects a number, and the
 player has to guess it. Provide hints like "too high"
 or "too low" to help the player guess the correct
-number. Also, display the number of attempts the
+number. Also, display the number of attempts the player took to guess the number.
 */
 
 #include <iostream>
